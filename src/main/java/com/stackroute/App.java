bdc0baaa66ba0c24722735dad033c7905f124e50
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-07-26 17:46:03 +0530

    Task1 except BeanDefinitionRegistry implementation

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -1,13 +1,24 @@
 package com.stackroute;
 
+import com.stackroute.domain.Manager;
+import com.stackroute.domain.Surveyor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext applicationContext=new ClassPathXmlApplicationContext("beans.xml");
+        Surveyor surveyor=applicationContext.getBean("b",Surveyor.class);
+        System.out.println(surveyor);
+
+        Manager manager=applicationContext.getBean("a",Manager.class);
+        System.out.println(manager);
+
     }
 }
--- src/main/java/com/stackroute/domain/Manager.java
+++ src/main/java/com/stackroute/domain/Manager.java
@@ -3,10 +3,14 @@ package com.stackroute.domain;
 public class Manager {
     private int manId;
     private String manName;
+    private Surveyor surveyor;
 
-    public Manager(int manId, String manName) {
+
+    public Manager(int manId, String manName,Surveyor surveyor) {
         this.manId = manId;
         this.manName = manName;
+        this.surveyor=surveyor;
+
     }
 
     public Manager() {
@@ -28,11 +32,21 @@ public class Manager {
         this.manName = manName;
     }
 
+
+    public Surveyor getSurveyor() {
+        return surveyor;
+    }
+
+    public void setSurveyor(Surveyor surveyor) {
+        this.surveyor = surveyor;
+    }
+
     @Override
     public String toString() {
         return "Manager{" +
                 "manId=" + manId +
                 ", manName='" + manName + '\'' +
+                ", surveyor=" + surveyor +
                 '}';
     }
 }
